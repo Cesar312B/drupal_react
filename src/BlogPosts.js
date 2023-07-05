89a112ebd6f02916b8f5873fc780764dcc23d5ea
Cesar312B <86753747+Cesar312B@users.noreply.github.com>
2023-07-05 16:57:11 -0500

    Update BlogPosts.js
    mejora al visualizar el post

--- src/BlogPosts.js
+++ src/BlogPosts.js
@@ -1,39 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import DOMPurify from 'dompurify';
 
 const BlogPosts = () => {
-  const [content, setContent] = useState({});
+    const [content, setContent] = useState({});
+    const contentRef = useRef(null);
 
-  useEffect(() => {
-    const fetchContent = async () => {
-      try {
-        const node = 1; // Reemplaza con el ID numérico del nodo que deseas obtener
-        const response = await axios.get(`http://localhost/drupal/node/${node}?_format=json`);
-        console.log(response.data); 
-        setContent(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+    useEffect(() => {
+        const fetchContent = async () => {
+          try {
+            const node = 1;
+            const response = await axios.get(`http://localhost/drupal/node/${node}?_format=json`);
+            console.log(response.data); 
+            setContent(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchContent();
+      }, []);
+    
+      useEffect(() => {
+        if (content.body && contentRef.current) {
+          const sanitizedHTML = DOMPurify.sanitize(content.body[0].processed);
+          contentRef.current.innerHTML = sanitizedHTML;
+        }
+      }, [content]);
+
+
+      return (
+        <div>
+          <h2>Post</h2>
+          <ul>
+            <li>
+              {content.title && (
+                <p>{content.title[0].value}</p>
+              )}
+              <div ref={contentRef}></div>
+            </li>
+          </ul>
+        </div>
+      );
     };
-
-    fetchContent();
-  }, []);
-
-  return (
-    <div>
-      <h2>Content List</h2>
-      <ul>
-        <li>
-          {content.title && (
-            <p>{content.title[0].value}</p>
-          )}
-          {content.body && (
-            <p>{content.body[0].processed}</p>
-          )}
-        </li>
-      </ul>
-    </div>
-  );
-};
 
 export default BlogPosts;
